d34d1d7ec8c16808c55603aed254ef1e3cf7b9b1
roy989898 <roy989898@gmail.com>
2015-11-19 23:34:18 +0800

    Finish exp1 vary delay
    0-300 step=10

--- parkinglot-sweep.sh
+++ parkinglot-sweep.sh
@@ -45,18 +45,23 @@ bw=100
 #        -o $dir/cwnd.png
 #done
 
-#sudo python parkinglot.py --bw 10 --dir test -t 60 
-dir=$rootdir/exp1
+#sudo python parkinglot.py --bw 10 --dir test -t 60
+#exp1 vary delay
+for i in {0..300..10}
+do
+    d_delay=$i
+    echo $d_delay
+    dir=$rootdir/exp1/vary_delay/$d_delay
     python parkinglot.py --bw $bw \
         --dir $dir \
         -t 30 \
-		--de 1 \
+	--de $d_delay \
       
     python util/plot_rate.py --rx \
         --maxy $bw \
         --xlabel 'Time (s)' \
         --ylabel 'Rate (Mbps)' \
-        -i 's[123456789]-eth[123456789]' \
+        -i 's[1]-eth[2]' \
         -f $dir/bwm.txt \
         -o $dir/rate_eth2.png
    
@@ -64,7 +69,10 @@ dir=$rootdir/exp1
     python util/plot_tcpprobe.py \
         -f $dir/tcp_probe.txt \
         -o $dir/cwnd.png
-		
+
+done	 
+
+#exp1 vary delay		
 	
 		
  
