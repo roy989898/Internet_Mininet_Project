ebbec392d55b203d01c7270bbd902ed21a4f3694
roy989898 <roy989898@gmail.com>
2015-11-22 18:13:29 +0800

    annd exp3 into the .sh file

--- parkinglot-sweep.sh
+++ parkinglot-sweep.sh
@@ -30,6 +30,7 @@ bw=100
 
 #sudo python parkinglot.py --bw 10 --dir test -t 60
 #exp1 change link h1-s1 vary delay  0ms to 300ms step=10
+#h1 send to h2
 for i in {0..300..30}
 do
     d_delay=$i
@@ -85,6 +86,7 @@ done
 #exp1 vary loss rate
 
 #exp2 change link s1-s2 vary delay
+#h1 send to h3
 for i in {0..300..30}
 do
     d_delay=$i
@@ -137,6 +139,62 @@ do
 done	
 	
 #exp2 vary loss rate
+
+#exp3 change link h1-s1 vary delay  0ms to 300ms step=10
+#h1 send to h3
+for i in {0..300..30}
+do
+    d_delay=$i
+    echo $d_delay
+    dir=$rootdir/exp1/vary_delay/$d_delay
+    python exp1.py --bw $bw \
+        --dir $dir \
+        -t 60 \
+	--de $d_delay \
+      
+    python util/plot_rate.py --rx \
+        --maxy $bw \
+        --xlabel 'Time (s)' \
+        --ylabel 'Rate (Mbps)' \
+        -i 's[1]-eth[2]' \
+        -f $dir/bwm.txt \
+        -o $dir/rate_eth2.png
+   
+		
+    python util/plot_tcpprobe.py \
+        -f $dir/tcp_probe.txt \
+        -o $dir/cwnd.png
+
+done	 
+
+#exp3 vary delay	
+
+#exp3 vary loss rate	
+for i in {0..8..2}
+do
+    loss_rate=$i
+    echo $loss_rate
+    dir=$rootdir/exp3/vary_lossrate/$loss_rate
+    python exp3.py --bw $bw \
+        --dir $dir \
+        -t 60 \
+		--lo $loss_rate \
+      
+    python util/plot_rate.py --rx \
+        --maxy $bw \
+        --xlabel 'Time (s)' \
+        --ylabel 'Rate (Mbps)' \
+        -i 's[1]-eth[2]' \
+        -f $dir/bwm.txt \
+        -o $dir/rate_eth2.png
+   
+		
+    python util/plot_tcpprobe.py \
+        -f $dir/tcp_probe.txt \
+        -o $dir/cwnd.png
+done	
+	
+#exp3 vary loss rate
 echo "Started at" $start
 echo "Ended at" `date`
 echo "Output saved to $rootdir"
